862963f4278f0c034019750d96b1265c2664ed38
shirishakadiri <shirishak04@gmail.com>
2021-04-21 13:51:32 -0400

    created a service layer to implement CRUD operation

---
+++ src/main/java/com/shirisha/restapi/service/CustomerService.java
@@ -0,0 +1,59 @@
+package com.shirisha.restapi.service;
+
+import com.shirisha.restapi.model.Customer;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class CustomerService {
+
+    private int customerIdCount = 1;
+    private List<Customer> customerList = new CopyOnWriteArrayList<>();
+
+    public Customer addCustomer(Customer customer){
+        customer.setCustomerId(customerIdCount);
+        customerList.add(customer);
+        customerIdCount++;
+        return customer;
+    }
+
+    public List<Customer> getCustomerList(){
+        return customerList;
+    }
+
+    public Customer getCustomer(int customerId){
+        return customerList
+                .stream()
+                .filter(c -> c.getCustomerId() == customerId)
+                .findFirst()
+                .get();
+    }
+
+    public Customer updateCustomer(int customerId, Customer customer){
+        customerList
+                .stream()
+                .forEach(c -> {
+                    if(c.getCustomerId() == customerId){
+                        c.setCustomerFirstName(customer.getCustomerFirstName());
+                        c.setCustomerLastName(customer.getCustomerLastName());
+                        c.setCustomerEmail(customer.getCustomerEmail());
+                    }
+                });
+
+        return customerList
+                .stream()
+                .filter(c -> c.getCustomerId() == customerId)
+                .findFirst()
+                .get();
+    }
+
+    public void deleteCustomer(int customerId){
+        customerList
+                .stream()
+                .forEach(c -> {
+                    if(c.getCustomerId() == customerId){
+                        customerList.remove(c);
+                    }
+                });
+    }
+}
